a0fe0595f15846a009e4606e1dac5d4ab59b7ece
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-01-17 18:59:33 -0500

    While I did easily write up standard prefix suffix probability matrix, I could not quite figure out how to adapt it to a triple nested dictionary

--- InstantChef!.playground/Contents.swift
+++ InstantChef!.playground/Contents.swift
@@ -13,3 +13,45 @@ let delimiters = NSCharacterSet(charactersInString: ", \n")
 
 // Break the string
 let words = content.componentsSeparatedByCharactersInSet(delimiters)
+
+
+// PROBABILITY GENERATION
+
+// Make dictionaries for prefix, suffix, and postSuffix
+var postSuffix = [String: Int] ()
+var suffix = [String: [String: Int]] ()
+var prefix = [String: [String: [String: Int] ] ] ()
+// Loop over input words
+for (index, word) in words.enumerate() {
+    
+    // Stop loop before no suffix remains
+    if index == words.count - 1 {
+        break
+    }
+    
+    // when the current word from the input array is not in the prefix dictionary at all & skip blank entries
+    if prefix[word] == nil && word != " " {
+        
+        postSuffix[words[index + 2]] = 1
+        
+        suffix[words[index + 1]] = postSuffix   // add a count of 1 for the suffix (word following current word in input)
+        postSuffix["📐"] = 1                // triangular ruler key represents total # of suffixs count
+        prefix[word] = suffix           // current word from input array becomes key in prefix dictionary with value of suffix dictionary just created
+        
+    } else {
+        
+        // add 1 to total in suffix dictionary
+        prefix[word]!["📐"]! += 1
+        
+        // does current word exist as a suffix already?
+        if suffix[word]![words[index + 1]] == nil {         // does not exist, so create and set to 1
+            suffix[word]![words[index + 1]] = 1
+            
+        } else {                                            // does exist, so increment by 1
+            suffix[word]![words[index + 1]]! += 1
+        }
+    }
+    
+    // wipe out the suffix dictionary for next iteration, so it starts blank
+    postSuffix = [:]
+}
