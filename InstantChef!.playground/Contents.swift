57a64cf686edcfe806e5127186bef84bcf42d8c7
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-01-17 18:10:39 -0500

    Added Source document, and code to acquire and break it into a dictionary

--- InstantChef!.playground/Contents.swift
+++ InstantChef!.playground/Contents.swift
@@ -1,2 +1,15 @@
 
 import Cocoa
+
+// Set file location
+let fileURL = NSBundle.mainBundle().URLForResource("Recipes2", withExtension: "txt")
+
+// make the file a string
+var content: String = ""
+content = try String(contentsOfURL: fileURL!, encoding: NSUTF8StringEncoding)
+
+// Set delimiters
+let delimiters = NSCharacterSet(charactersInString: ", \n")
+
+// Break the string
+let words = content.componentsSeparatedByCharactersInSet(delimiters)
