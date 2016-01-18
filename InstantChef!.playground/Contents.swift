ae1d35d32bf3f549b38b83893b7b2d63e27e4309
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-01-17 19:47:02 -0500

    Implemented basic markov probabilities

--- InstantChef!.playground/Contents.swift
+++ InstantChef!.playground/Contents.swift
@@ -17,13 +17,12 @@ let words = content.componentsSeparatedByCharactersInSet(delimiters)
 
 // PROBABILITY GENERATION
 
-// Make dictionaries for prefix, suffix, and postSuffix
-var postSuffix = [String: Int] ()
-var suffix = [String: [String: Int]] ()
-var prefix = [String: [String: [String: Int] ] ] ()
+// Make dictionaries for preprefix, prefix, and suffix
+var suffix = [String: Int] ()
+var prefix = [String: [String: Int]] ()
 // Loop over input words
 for (index, word) in words.enumerate() {
-    
+
     // Stop loop before no suffix remains
     if index == words.count - 1 {
         break
@@ -32,26 +31,28 @@ for (index, word) in words.enumerate() {
     // when the current word from the input array is not in the prefix dictionary at all & skip blank entries
     if prefix[word] == nil && word != " " {
         
-        postSuffix[words[index + 2]] = 1
-        
-        suffix[words[index + 1]] = postSuffix   // add a count of 1 for the suffix (word following current word in input)
-        postSuffix["📐"] = 1                // triangular ruler key represents total # of suffixs count
+        suffix[words[index + 1]] = 1    // add a count of 1 for the suffix (word following current word in input)
+        suffix["📐"] = 1                // represents total # of suffixs count
         prefix[word] = suffix           // current word from input array becomes key in prefix dictionary with value of suffix dictionary just created
         
     } else {
         
         // add 1 to total in suffix dictionary
-        prefix[word]![words[index+1]]!["📐"]! += 1
+        prefix[word]!["📐"]! += 1
         
         // does current word exist as a suffix already?
-        if suffix[word]![words[index + 1]] == nil {         // does not exist, so create and set to 1
-            suffix[word]![words[index + 1]] = 1
+        if prefix[word]![words[index + 1]] == nil {         // does not exist, so create and set to 1
+            prefix[word]![words[index + 1]] = 1
             
         } else {                                            // does exist, so increment by 1
-            suffix[word]![words[index + 1]]! += 1
+            prefix[word]![words[index + 1]]! += 1
         }
     }
     
     // wipe out the suffix dictionary for next iteration, so it starts blank
-    postSuffix = [:]
+    suffix = [:]
 }
+
+prefix
+
+
