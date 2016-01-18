ae26fa1bc6787000defeeb0ac985b3fdfa076548
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-01-17 20:36:11 -0500

    got the output rigged up

--- InstantChef!.playground/Contents.swift
+++ InstantChef!.playground/Contents.swift
@@ -1,6 +1,8 @@
 
 import Cocoa
 
+//INPUT TEXT
+
 // Set file location
 let fileURL = NSBundle.mainBundle().URLForResource("Recipes2", withExtension: "txt")
 
@@ -15,6 +17,7 @@ let delimiters = NSCharacterSet(charactersInString: ", \n")
 let words = content.componentsSeparatedByCharactersInSet(delimiters)
 
 
+
 // PROBABILITY GENERATION
 
 // Make dictionaries for preprefix, prefix, and suffix
@@ -22,37 +25,115 @@ var suffix = [String: Int] ()
 var prefix = [String: [String: Int]] ()
 // Loop over input words
 for (index, word) in words.enumerate() {
-
-    // Stop loop before no suffix remains
+    word
+    index
+    // Stop loop if no suffix remains
     if index == words.count - 1 {
         break
     }
     
-    // when the current word from the input array is not in the prefix dictionary at all & skip blank entries
+    //the prefix is not a space and is not in the dictionary just yet
     if prefix[word] == nil && word != " " {
         
         suffix[words[index + 1]] = 1    // add a count of 1 for the suffix (word following current word in input)
-        suffix["📐"] = 1                // represents total # of suffixs count
+        suffix["#️⃣"] = 1                // triangular ruler key represents total # of suffixs count
         prefix[word] = suffix           // current word from input array becomes key in prefix dictionary with value of suffix dictionary just created
         
+        //otherwise just add to the suffix dictionary and move on
     } else {
-        
-        // add 1 to total in suffix dictionary
-        prefix[word]!["📐"]! += 1
-        
+        prefix[word]!["#️⃣"]! += 1 // add 1 to total suffixes
         // does current word exist as a suffix already?
-        if prefix[word]![words[index + 1]] == nil {         // does not exist, so create and set to 1
+        if prefix[word]![words[index + 1]] == nil {         //create and set to 1
             prefix[word]![words[index + 1]] = 1
             
-        } else {                                            // does exist, so increment by 1
+        } else {                                            //add 1
             prefix[word]![words[index + 1]]! += 1
         }
     }
-    
-    // wipe out the suffix dictionary for next iteration, so it starts blank
+    //wipe for a fresh set of suffixes
     suffix = [:]
 }
 
-prefix
+
+
+//OUTPUT GENERATION
+
+
+//select seed word at random from the input words
+var currentWord = words[Int(arc4random_uniform(UInt32(words.count)))]
+var Recipe: String = currentWord + " "    // start the output sentence
+var end: Bool = false
+
+
+//make 20 sentences
+
+    
+//loop until complete sentence
+while(true) {
+    //for _ in 1...20 {
+    
+    //Ensure the presence of a valid seed word
+    if prefix[currentWord] != nil && currentWord != " " {
+        
+        // Generate the random value
+        let randVal = Float(arc4random_uniform(1000000)) / 10000
+        randVal
+        
+        // Stores upper value of probabi lity for current suffix word
+        var upVal: Float = 0
+        
+        // iterate over all suffix words for this prefix
+        for (potSuf, count) in prefix[currentWord]! {
+            
+            // get total suffix words for this prefix
+            let suffixTotal = prefix[currentWord]!["#️⃣"]!
+            
+            // exclude the instance of the suffix that contains the suffix total
+            if potSuf != "#️⃣" {
+                
+                //get upper value
+                upVal += Float(count) / Float( suffixTotal ) * 100
+                
+                //Check if suffix is eligible for use
+                if (randVal < upVal) {
+                    
+                    // add the suffix to output
+                    Recipe += potSuf
+                    
+                    // make the suffix the prefix
+                    currentWord = potSuf
+                    
+                    //Check for end of sentence
+                    if Recipe[Recipe.endIndex.predecessor()] == "." {
+                        end = true
+                    }
+                    
+                    // Add space before next word
+                    Recipe += " "
+                    
+                    break
+                }
+            }
+            
+        }
+        
+    } else {
+        //If it can't find a seed word
+        Recipe = "Seed Word Not Found! 😢"
+        
+        // Stop building the output sentence.
+        break
+    }
+    
+    // Stop building the sentence if last word added has a period
+    if (end == true) {
+        break
+    }
+    
+}
+
+
+Recipe
+
 
 
